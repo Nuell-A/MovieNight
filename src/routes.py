f4a1f1956cd1bfebb33df52c7d3a4026af08f5cc
Emanuel Alcala-Arredondo <alcala.arredondo1@gmail.com>
2024-06-12 08:17:17 -0700

    Implemented POST requests when creating/joining rooms to redirect to the correct page if create/join room is a success.

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from flask_socketio import emit, join_room
 from . import app, socketio
 
@@ -9,9 +9,31 @@ rooms = {}
 def index():
     return render_template('index.html')
 
-@app.route('/room')
+@app.route('/room', methods=['GET', 'POST'])
 def room():
-    return render_template('room.html')
+    if request.method == 'POST':
+        # Retrieve POST data.
+        data = request.json
+        room = data['room']
+        room_type = data['type']
+        
+        if room_type == 'create':
+            if room in rooms:
+                
+                return jsonify({'status': 'error', 'message': 'Room already exists.'})
+            print(f"POST REQUEST: Created room {room}")
+        elif room_type == 'join':
+            if room not in rooms:
+                return jsonify({'status': 'error', 'message': 'Room does not exist.'})
+            print(f"POST REQUEST: Joined room {room}")
+        
+        return jsonify({'status': 'success', 'room': room})
+    elif request.method == 'GET':
+        room = request.args.get('room')
+        # Prevents accessing room page directly.
+        if (room == None):
+            return render_template('index.html')
+        return render_template('room.html', room=room)
 
 @socketio.on('join')
 def handle_join(data):
@@ -25,9 +47,9 @@ def handle_join(data):
             join_room(room)
             print("Created room...")
             emit('join', {'type': 'create', 'userId': userId, 'room': room}, room=room)
+            print(rooms)
         else:
             print("Already created.")
-            return
     elif (socket_type == "join"):
         if (room not in rooms):
             print("Room does not exist")
@@ -36,8 +58,7 @@ def handle_join(data):
             join_room(room)
             print("Joined room...")
             emit('join', {'type': 'join', 'userId': userId, 'room': room}, room=room)
-    print(rooms)
-    
+            print(rooms)
 
 @socketio.on('message')
 def handle_message(data):
